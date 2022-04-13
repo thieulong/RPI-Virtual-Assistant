40a490db2f98c29bf72357b614e4a561c790fecd
thieulong <thieulongph@gmail.com>
2022-04-13 23:20:46 +0700

    Working on

--- professor_virtual_assistant.py
+++ professor_virtual_assistant.py
@@ -5,8 +5,13 @@ import pvporcupine
 import subprocess
 from pydub import AudioSegment
 from pydub.playback import play
-import speech_recognition
+import speech_recognition       
 from pixels import pixels
+from grove.factory import Factory
+from youtube_search import YoutubeSearch
+import os
+import webbrowser
+import time
 
 porcupine = None
 pa = None
@@ -17,11 +22,35 @@ speech = speech_recognition.Microphone(device_index=4)
 
 chime = AudioSegment.from_wav("chime.wav")
 
+pixels.off()
+
+light = Factory.getGpioWrapper("Relay",24)
+
+os.system("clear")
+
 def execute_unix(text):
     command = 'espeak -ven+m3 -k5 -s150 --punct="<characters>" "%s" 2>>/dev/null' % text
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     return output
+
+def light_control(text):
+    if any(word in text for word in ["on", "activate"]):
+        light.on()
+        execute_unix("The light is on!")
+    elif any(word in text for word in ["off", "deactivate"]):
+        light.off()
+        execute_unix("The light is off!")
+        
+def play_youtube(text):
+    links = list()
+    keyword = text[text.find('for')+4:]
+    results = YoutubeSearch(keyword).to_dict()
+    for link in results: links.append('https://www.youtube.com' + link['url_suffix'])
+    print("Playing '{}' on Youtube!".format(keyword))
+    execute_unix("Playing {} on Youtube!".format(keyword))
+    url = links[0]
+    webbrowser.open(url=url)
 
 try:
     porcupine = pvporcupine.create(access_key='OBarq6+nQZjftCadhlX1ZR1WulknlRl2PzkUEmxYU7RBZLYTP2g6QQ==',
@@ -37,27 +66,43 @@ try:
                     frames_per_buffer=porcupine.frame_length)
 
     while True:
+        
         pcm = audio_stream.read(porcupine.frame_length)
         pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
 
         keyword_index = porcupine.process(pcm)
 
         if keyword_index == 0:
-            print("Hotword Detected")
+            print("Professor wake word detected!\n")
             pixels.wakeup()
-            play(chime)
-            # execute_unix("Yes")
             with speech as source:
                 audio = rec.adjust_for_ambient_noise(source)
+                play(chime)
+                execute_unix("Yes")
                 audio = rec.listen(source)
             try:
                 text = rec.recognize_google(audio, language = 'en-US')
-                print(text)
+                print("Command: {}\n".format(text))
             except speech_recognition.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
+                execute_unix("Sorry, I didn't quite hear you. Can you please repeat?")
+                pixels.off()
             except speech_recognition.RequestError as error:
-                print("Could not request results from Google Speech Recognition service; {0}".format(error))
+                # print("Could not request results from Google Speech Recognition service; {0}".format(error))
+                execute_unix("Sorry, I am currently under maintenance. Please wait a momment!")
+                pixels.off()
+            else:
+                if any(word in text for word in ["light", "lights", "bulb", "LED"]):
+                    light_control(text=text)
+                    pixels.off()
+                    continue
+                if any(word in text for word in ["YouTube"]):
+                    play_youtube(text=text)
+                    pixels.off()
+                    continue
+                
+                pixels.off()
 finally:
     if porcupine is not None:
         porcupine.delete()
+        
     
